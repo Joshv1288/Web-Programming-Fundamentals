8a997b0f2eac23b02199ed3dcd1ec646d106f148
Joshua Velez <joshv1288@gmail.com>
2013-07-17 21:20:15 -1000

    started second problem

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -1,10 +1,18 @@
 //Expression Worksheet
 
+//Dog Years
+
 //Sparky's age
-var age="4"
-var humanYears
+var age="4";
+var humanYears;
 
 //Expression
-
-humanYears = age * 7
+humanYears = age * 7;
 console.log("Sparky is" +" "+ humanYears +" "+ "human years old which is" +" "+ age +" "+ "in dog years.")
+
+
+//Slice of Pie I
+
+var slices;
+var people;
+var pizzas;

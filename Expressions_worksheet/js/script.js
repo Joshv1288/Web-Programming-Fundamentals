d790f3b1faa7c45ad0ef9b337b5c0837e5a31cc7
Joshua Velez <joshv1288@gmail.com>
2013-07-17 21:25:25 -1000

    Added space between varibles and tested expression 2

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -3,7 +3,7 @@
 //Dog Years
 
 //Sparky's age
-var age="4";
+var age = "4";
 var humanYears;
 
 //Expression
@@ -13,7 +13,10 @@ console.log("Sparky is" +" "+ humanYears +" "+ "human years old which is" +" "+ 
 
 //Slice of Pie I
 
-var slices="8";
-var people="15";
-var pizza="10";
+var slices = "8";
+var people = "15";
+var pizza = "10";
+var slicesPer;
 
+//Expression
+slicesPer = pizza * slices / people

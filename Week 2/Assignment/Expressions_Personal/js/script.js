22feaa42a70ed7cc6dd149f3c57e08b9296973f0
Joshua Velez <joshv1288@gmail.com>
2013-07-18 15:47:39 -1000

    Added extra variable to compensate for decimal

--- Week 2/Assignment/Expressions_Personal/js/script.js
+++ Week 2/Assignment/Expressions_Personal/js/script.js
@@ -6,10 +6,13 @@
 // Calculating Interest earned
 
 var amountSaving = 100
-var months = 5
+var months = 6
 var interest = .06
 var totalEarned
+var interestEarned
 
 totalEarned = amountSaving * months * interest
+
+//interestEarned = totalEarned / 10
 
 console.log(totalEarned)

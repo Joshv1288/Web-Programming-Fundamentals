c76543a89459297785e3cfc65d03666e2d2d566c
Joshua Velez <joshv1288@gmail.com>
2013-07-18 16:29:34 -1000

    Setup variables and array

--- Week 2/Assignment/Expressions_Industry/js/script.js
+++ Week 2/Assignment/Expressions_Industry/js/script.js
@@ -1,3 +1,14 @@
 // Joshua Velez
 // July 17, 2013
-//Expressions
+// Industry Expressions
+
+
+//Calculating APFT Score
+var pushUps
+var sitUps
+var runTime
+
+var scores = new Array();
+scores[0]= var pushUps
+scores[1]= var sitUps
+scores[2]= var runTime

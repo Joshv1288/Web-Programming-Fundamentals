03ebcfecca42f0452eb450563787d63816d35cd6
Joshua Velez <joshv1288@gmail.com>
2013-07-18 16:42:46 -1000

    added semicolons

--- Week 2/Assignment/Expressions_Industry/js/script.js
+++ Week 2/Assignment/Expressions_Industry/js/script.js
@@ -7,15 +7,15 @@
 
 alert("Please have Raw Score already calculated to Simple Score")
 
-var pushUps = 76
-var sitUps = 84
-var runTime = 80
-var totalScore
+var pushUps = 76;
+var sitUps = 84;
+var runTime = 80;
+var totalScore;
 
-var scores = new Array();
-scores[0]= var pushUps
-scores[1]= var sitUps
-scores[2]= var runTime
+var scores = array();
+scores[0]= var pushUps;
+scores[1]= var sitUps;
+scores[2]= var runTime;
 
-totalScore ++ scores[0] + scores[1] + scores[2]
+totalScore += scores[0] + scores[1] + scores[2]
 console.log(totalScore)

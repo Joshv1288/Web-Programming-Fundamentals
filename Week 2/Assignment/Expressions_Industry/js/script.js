a01d1616211d1352f15df140db05ca8f1c7d48e5
Joshua Velez <joshv1288@gmail.com>
2013-07-18 16:52:35 -1000

    Added prompts

--- Week 2/Assignment/Expressions_Industry/js/script.js
+++ Week 2/Assignment/Expressions_Industry/js/script.js
@@ -7,17 +7,18 @@
 
 alert("Please have Raw Score already calculated to Simple Score")
 
-var pushUps = 76;
-var sitUps = 84;
-var runTime = 80;
+var pushUps = prompt("Please Enter Push Up Score")
+var sitUps = prompt("Please Enter Sit Up Score")
+var runTime = prompt("Please Enter Run Score")
 var totalScore;
 
-var scores = [var pushUps, var sitUps, var runTime];
+//var scores = [var pushUps, var sitUps, var runTime];
 
 //var scores = new Array();
 //scores[0]= var pushUps;
 //scores[1]= var sitUps;
 //scores[2]= var runTime;
 
-totalScore += scores[0] + scores[1] + scores[2];
+totalScore = pushUps + sitUps +runTime;
 console.log(totalScore);
+alert("Your Total APFT Score is " + totalScore)

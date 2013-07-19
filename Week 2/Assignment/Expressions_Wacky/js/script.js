4b2b8ee26647a54fa8acb06902aa13adefa5b798
Joshua Velez <joshv1288@gmail.com>
2013-07-18 18:45:49 -1000

    Added variables and alert

--- Week 2/Assignment/Expressions_Wacky/js/script.js
+++ Week 2/Assignment/Expressions_Wacky/js/script.js
@@ -2,3 +2,11 @@
 // July 17, 2013
 // Wacky Expressions
 
+
+//Reading Rate
+
+var wordsPassage = prompt("Enter the number of words read") // Number of words you are reading
+var time prompt("Enter the time it took you to read in seconds") // How long it took you to read in seconds
+var wpm = (wordsPassage / time) * 60 // Expression
+
+alert("You read " + wpm + " words per minute.")

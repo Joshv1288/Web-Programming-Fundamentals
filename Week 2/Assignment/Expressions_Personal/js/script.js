324e4c0d34baa9c419a8dc7c547c903648376580
Joshua Velez <joshv1288@gmail.com>
2013-07-18 16:11:53 -1000

    added comments for lines

--- Week 2/Assignment/Expressions_Personal/js/script.js
+++ Week 2/Assignment/Expressions_Personal/js/script.js
@@ -5,14 +5,14 @@
 
 // Calculating Interest earned
 
-var amountSaving = prompt("Please enter the amount you will be saving each month")
-var months = prompt("Enter number of months you will be saving")
-var interest = prompt("Enter your interest rate in decimal form")
+var amountSaving = prompt("Please enter the amount you will be saving each month") //Collects amount user will be saving
+var months = prompt("Enter number of months you will be saving") //Collects number of months the user will save
+var interest = prompt("Enter your interest rate in decimal form")//Collects interest rate of user
 var totalEarned
 var interestEarned
 
 
-totalEarned = amountSaving * months * interest
-interestEarned = totalEarned / 10
+totalEarned = amountSaving * months * interest //Expression to calculate earned
+interestEarned = totalEarned / 10//Expression to move decimal one point to the left
 
-alert("You will earn $" + interestEarned + " over " + months + "s of saving.")
+alert("You will earn $" + interestEarned + " over " + months + " months of saving.")//Alert telling user there total interest earned

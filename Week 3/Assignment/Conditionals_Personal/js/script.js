694f0c0c7c723e4e1612730bcbd119d20ebbf6ae
Joshua Velez <Joshv1288@gmail.com>
2013-07-24 20:24:55 -1000

    Added mimimum score variable

--- Week 3/Assignment/Conditionals_Personal/js/script.js
+++ Week 3/Assignment/Conditionals_Personal/js/script.js
@@ -5,15 +5,12 @@
 
 //If Pushups, Situps and Runtime are greater than or equal to 60 then you passed the PT test
 
-var rawPushup = 80
-var rawSitup = 88
-var rawRunTme = 89
+var rawPushup = 80;
+var rawSitup = 88;
+var rawRunTme = 89;
 
-if(rawPushup => 60 && rawSitup => 60){
-
-	console.log("You passed the PT test!")
-
-}else{
-
-	console.log("You failed the PT test! :(")
+if(rawPushup > parseInt(60) && rawSitup > parseInt(60)){
+	console.log("You passed the PT test!");
+else{
+	console.log("You failed the PT test! :(");
 }

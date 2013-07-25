49bdd1e27b1c1d02c36faa481c8e7176583f11e3
Joshua Velez <Joshv1288@gmail.com>
2013-07-24 20:14:57 -1000

    Added variables and testing conditional

--- Week 3/Assignment/Conditionals_Personal/js/script.js
+++ Week 3/Assignment/Conditionals_Personal/js/script.js
@@ -3,4 +3,17 @@
 //Conditional Personal
 
 
+//If Pushups, Situps and Runtime are greater than or equal to 60 then you passed the PT test
 
+var rawPushup = 60
+var rawSitup = 55
+var rawRunTme = 89
+
+if(rawPushup >= 60 && rawSitup >= 60 && rawRunTme >= 60){
+
+	console.log("You passed the PT test!")
+
+}else {
+
+	console.log("You failed the PT test! :(")
+}

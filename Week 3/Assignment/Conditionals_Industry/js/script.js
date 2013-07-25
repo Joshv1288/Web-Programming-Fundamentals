5c5974808bd8533da79f6b5ff34bdec4dab9b5c3
Joshua Velez <Joshv1288@gmail.com>
2013-07-24 20:19:34 -1000

    Test keeps failing trying differnet things

--- Week 3/Assignment/Conditionals_Industry/js/script.js
+++ Week 3/Assignment/Conditionals_Industry/js/script.js
@@ -5,15 +5,15 @@
 
 //If Pushups, Situps and Runtime are greater than or equal to 60 then you passed the PT test
 
-var rawPushup = 80
-var rawSitup = 88
+var rawPushup = 60
+var rawSitup = 55
 var rawRunTme = 89
 
-if(rawPushup => 60 && rawSitup => 60){
+if(rawPushup >= 60 && rawSitup >= 60 && rawRunTme >= 60){
 
 	console.log("You passed the PT test!")
 
-}else{
+}else {
 
 	console.log("You failed the PT test! :(")
 }

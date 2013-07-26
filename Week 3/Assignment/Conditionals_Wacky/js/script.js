5d2bbe0f911f0e609c23975efdc424d401bd79ea
Joshua Velez <Joshv1288@gmail.com>
2013-07-25 17:04:40 -1000

    Changed variables and conditional to ternary

--- Week 3/Assignment/Conditionals_Wacky/js/script.js
+++ Week 3/Assignment/Conditionals_Wacky/js/script.js
@@ -2,16 +2,9 @@
 //
 //
 
-var monthBirth = prompt("Please enter month of birth in number form")
-var yearBirth = prompt("Please enter Year of Birth in number form")
+var yearBorn = prompt("Please enter the year you were born.")
+var currentYear = prompt("Please enter the current year.")
 
+var drinkAge = currentYear - yearBorn
 
-
-if(yearBirth >= 1992){
-	if(monthbirth >=date.getMonth()){
-		console.log("You are old enough to drink alchol!!")
-	}
-		
-}else{
-	console.log("You can NOT drink alchol")
-}
+drinkAge >= 21 ? alert("You are " + drinkAge + " years old. So you can drink alchol.") : alert("You are " + drinkAge + " years old. So you can NOT drink alchol.")

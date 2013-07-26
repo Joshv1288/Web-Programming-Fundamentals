d5cd85aadcaba355590c2bb51b95a4beb87ad26c
Joshua Velez <Joshv1288@gmail.com>
2013-07-25 15:22:09 -1000

    replace console.log with alerts

--- Week 3/Assignment/Conditionals_Industry/js/script.js
+++ Week 3/Assignment/Conditionals_Industry/js/script.js
@@ -6,7 +6,8 @@ var minimumScore = 60;
 //If Pushups, Situps and Runtime are greater than or equal to 60 then you passed the PT test
 
 if(rawPushup >= minimumScore && rawSitup >= minimumScore && rawRunTime >= minimumScore){
-console.log("You passed the PT test!") ;
+alert("You passed the PT test!") ;
 }else{
-console.log("You failed the PT test!");
+alert("You failed the PT test!");
 }
+

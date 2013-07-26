e35f33fb7a2ff49c35b9a10a642bcbf9a74143cc
Joshua Velez <Joshv1288@gmail.com>
2013-07-25 16:57:06 -1000

    added variables and conditionals

--- Week 3/Assignment/Conditionals_Wacky/js/script.js
+++ Week 3/Assignment/Conditionals_Wacky/js/script.js
@@ -1,3 +1,17 @@
 //
 //
 //
+
+var monthBirth = prompt("Please enter month of birth in number form")
+var yearBirth = prompt("Please enter Year of Birth in number form")
+
+
+
+if(yearBirth >= 1992){
+	if(monthbirth >=date.getMonth()){
+		console.log("You are old enough to drink alchol!!")
+	}
+		
+}else{
+	console.log("You can NOT drink alchol")
+}

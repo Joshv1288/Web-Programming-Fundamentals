4366c700ec5c5f942f1efd646e9c675a217850fd
Joshua Velez <Joshv1288@gmail.com>
2013-07-25 17:07:19 -1000

    add comments to lines

--- Week 3/Assignment/Conditionals_Wacky/js/script.js
+++ Week 3/Assignment/Conditionals_Wacky/js/script.js
@@ -2,9 +2,9 @@
 //Web Programming Fundamentals
 //Wacky Conditional
 
-var yearBorn = prompt("Please enter the year you were born.")
-var currentYear = prompt("Please enter the current year.")
+var yearBorn = prompt("Please enter the year you were born.") // Collects Year user was born
+var currentYear = prompt("Please enter the current year.") // Collects current year
 
-var drinkAge = currentYear - yearBorn
+var drinkAge = currentYear - yearBorn // Calculates the users ages
 
-drinkAge >= 21 ? alert("You are " + drinkAge + " years old. So you can drink alchol.") : alert("You are " + drinkAge + " years old. So you can NOT drink alchol.")
+drinkAge >= 21 ? alert("You are " + drinkAge + " years old. So you can drink alchol.") : alert("You are " + drinkAge + " years old. So you can NOT drink alchol.") // Displays whether or not the user can drink because age is greater than or equal to 21.

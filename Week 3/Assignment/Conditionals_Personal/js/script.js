d20f79a6940025cfca252c23c5daf5f1cc8de441
Joshua Velez <Joshv1288@gmail.com>
2013-07-25 16:13:43 -1000

    add condition for null value on prompt

--- Week 3/Assignment/Conditionals_Personal/js/script.js
+++ Week 3/Assignment/Conditionals_Personal/js/script.js
@@ -3,19 +3,12 @@
 //Conditional Personal
 
 
-var temp = 76
-var waveHeight = 2
+var waveHeight = prompt("What is the wave height at the beach?")
 var minimumHeight = 3
-var minimumTemp = 70
 
-if(temp > minimumTemp){
-	console.log("Little to chilly outside to surf")
-} else if(waveHeight > minimumHeight){
-	console.log("Lets Go surfing brah!!")
-} else {
-	console.log("Looking kinda flat brah")
+if (waveHeight == null) {
+	alert("Please enter the wave height. Please refresh the page")
 }
 
 
-
-//waveHeight > minimumHeight ? console.log("Lets go surfing!!") :  console.log("Looking kind of flat brah!")
+waveHeight > minimumHeight ? console.log("Lets go surfing!!") :  console.log("Looking kind of flat brah!")

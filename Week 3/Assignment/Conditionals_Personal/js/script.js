f6c2ac7ba79bcada4e1c3ea81f48d7d76fe226db
Joshua Velez <Joshv1288@gmail.com>
2013-07-25 16:39:20 -1000

    fixed windspeed variable

--- Week 3/Assignment/Conditionals_Personal/js/script.js
+++ Week 3/Assignment/Conditionals_Personal/js/script.js
@@ -8,7 +8,7 @@ var windSpeed = prompt("What is the wind speed?") // Collects wind speed
 var minimumHeight = 3 // Minimum wave height required for surfing
 var maximumWind = 20 //MaximumWind Speed to not surf
 
-if (waveHeight != '' || waveHeight != null) {
+/*if (waveHeight != '' || waveHeight != null) {
 	alert("Please enter the wave height. Please refresh the page")
 	console.log("please enter the wave height. Please refresh the page")
 }
@@ -16,12 +16,15 @@ if (waveHeight != '' || waveHeight != null) {
 if (windSpeed != '' || windSpeed != null) {
 	alert("Please enter the wind speed. Please refresh the page")
 	console.log("please enter the wind speed. Please refresh the page")
-}
+}*/
 
 if (waveHeight > minimumHeight){
-	if (windspeed < maximumWind) {
+	if (windSpeed < maximumWind) {
 		console.log("Lets go surfing")
+	}else{
+		console.log("Water is a little choppy")
 	}
+
 }else{
 	console.log("Looks really flat!")
 }

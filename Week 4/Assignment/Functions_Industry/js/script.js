0c652bf8fcc1012e56f61323b9c49807da5b7796
Joshua Velez <Joshv1288@gmail.com>
2013-07-31 19:00:56 -1000

    testing prompt in function

--- Week 4/Assignment/Functions_Industry/js/script.js
+++ Week 4/Assignment/Functions_Industry/js/script.js
@@ -7,8 +7,8 @@ Functions Industry
 //Determine PT Test Score
 
 function testScore(){
-	var pushUp = 80
-	var sitUp = 90
+	var pushUp =  prompt(How many pushups did you do?)
+		var sitUp = 90
 	var runTime = 85
 	var score = pushUp + sitUp + runTime
 	console.log(score)

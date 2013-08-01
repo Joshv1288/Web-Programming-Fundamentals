77039b611d5c134e162e84b18f6d86cf363fd5c9
Joshua Velez <Joshv1288@gmail.com>
2013-07-31 19:25:25 -1000

    Trying out an anonymous function

--- Week 4/Assignment/Functions_Industry/js/script.js
+++ Week 4/Assignment/Functions_Industry/js/script.js
@@ -10,19 +10,15 @@ var p =  prompt("How many pushups did you do?")
 var s = 90
 var t = 85
 
-testScore(p, s, t)
 
-function testScore(p, s, t){
+var testScore = function testScore(p, s, t){
 	var score = parseInt(p) + parseInt(s) + parseInt(t)
 	console.log(score)
 }
 
 
-if(p >= 60 && s >= 60 && t >=60){
-	
+if (p >= 60 && s >= 60 && t >=60){
 	console.log("You passed your PT Test with a score of " + score)
-	
-}esle{
-	
-	console.log("You failed the PT Test."
+}else{
+	console.log("You failed the PT Test.")
 }

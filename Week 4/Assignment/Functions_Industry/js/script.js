e9d0e169ae42cec380b0f0f56d50c29feac5f9ec
Joshua Velez <Joshv1288@gmail.com>
2013-07-31 18:59:50 -1000

    Testing Industry Function

--- Week 4/Assignment/Functions_Industry/js/script.js
+++ Week 4/Assignment/Functions_Industry/js/script.js
@@ -4,3 +4,14 @@ Web Programming Fundamentals
 Functions Industry
 */
 
+//Determine PT Test Score
+
+function testScore(){
+	var pushUp = 80
+	var sitUp = 90
+	var runTime = 85
+	var score = pushUp + sitUp + runTime
+	console.log(score)
+}
+
+testScore();

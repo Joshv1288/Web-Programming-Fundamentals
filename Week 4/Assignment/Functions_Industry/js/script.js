c15e10560b2d7bc2a5f9bb29318bffd1d4d15402
Joshua Velez <Joshv1288@gmail.com>
2013-07-31 19:54:44 -1000

    added alerts and comments

--- Week 4/Assignment/Functions_Industry/js/script.js
+++ Week 4/Assignment/Functions_Industry/js/script.js
@@ -6,22 +6,22 @@ Functions Industry
 
 //Determine PT Test Score
 
-var p = prompt("How many pushups did you do?");
-var s = prompt("How many situps did you do?");
-var t = prompt("How fast did you run?");
+var p = prompt("How many pushups did you do?"); // Gets users pushup score
+var s = prompt("How many situps did you do?"); // Gets users sit up score
+var t = prompt("How fast did you run?"); // Gets users runtime score
 
-testScore(p, s, t);
+testScore(p, s, t); // Calls function
 
 function testScore(p, s, t){
-	var score = parseInt(p) + parseInt(s) + parseInt(t);
-	console.log("Your final score is " + score.);
+	var score = parseInt(p) + parseInt(s) + parseInt(t); // Adds scores
+	alert("Your final score is " + score.); // Alerts user of final score
 }
 
 
 if (p >= 90 && s >= 90 && t >=90){
-	console.log("You passed your PT Test and earned the PT Badge.");
+	alert("You passed your PT Test and earned the PT Badge."); // Alerts user if they passed PT Test and got PT Badge.
 }else if (p >= 60 && s >= 60 && t >=60){
-	console.log("You passed the PT Test.");
+	alert("You passed the PT Test."); // Alerts user if they passed PT Test.
 }else{
-	console.log("You failed the PT Test.");
+	alert("You failed the PT Test."); // ALerts user if they failed PT Test.
 }

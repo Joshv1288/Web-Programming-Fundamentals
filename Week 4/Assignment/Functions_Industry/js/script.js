65e155df07aef59762cedbdbfebee525c550835d
Joshua Velez <Joshv1288@gmail.com>
2013-07-31 19:51:23 -1000

    added semi colons

--- Week 4/Assignment/Functions_Industry/js/script.js
+++ Week 4/Assignment/Functions_Industry/js/script.js
@@ -6,22 +6,22 @@ Functions Industry
 
 //Determine PT Test Score
 
-var p = prompt("How many pushups did you do?")
-var s = prompt("How many situps did you do?")
-var t = prompt("How fast did you run?")
+var p = prompt("How many pushups did you do?");
+var s = prompt("How many situps did you do?");
+var t = prompt("How fast did you run?");
 
-testScore(p, s, t)
+testScore(p, s, t);
 
 function testScore(p, s, t){
-	var score = parseInt(p) + parseInt(s) + parseInt(t)
-	console.log("Your final score is " + score)
+	var score = parseInt(p) + parseInt(s) + parseInt(t);
+	console.log("Your final score is " + score.);
 }
 
 
 if (p >= 90 && s >= 90 && t >=90){
-	console.log("You passed your PT Test and earned the PT Badge.")
+	console.log("You passed your PT Test and earned the PT Badge.");
 }else if (p >= 60 && s >= 60 && t >=60){
-	console.log("You passed the PT Test.")
+	console.log("You passed the PT Test.");
 }else{
-	console.log("You failed the PT Test.")
+	console.log("You failed the PT Test.");
 }

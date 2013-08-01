f005020ab8e4ca6544a38b8e57026e279737ab93
Joshua Velez <Joshv1288@gmail.com>
2013-07-31 19:50:06 -1000

    added prompts

--- Week 4/Assignment/Functions_Industry/js/script.js
+++ Week 4/Assignment/Functions_Industry/js/script.js
@@ -6,20 +6,20 @@ Functions Industry
 
 //Determine PT Test Score
 
-var p =  prompt("How many pushups did you do?")
-var s = 90
-var t = 90
+var p = prompt("How many pushups did you do?")
+var s = prompt("How many situps did you do?")
+var t = prompt("How fast did you run?")
 
 testScore(p, s, t)
 
 function testScore(p, s, t){
 	var score = parseInt(p) + parseInt(s) + parseInt(t)
-	return score
+	console.log("Your final score is " + score)
 }
 
 
 if (p >= 90 && s >= 90 && t >=90){
-	console.log("You passed your PT Test and earned the PT Badge")
+	console.log("You passed your PT Test and earned the PT Badge.")
 }else if (p >= 60 && s >= 60 && t >=60){
 	console.log("You passed the PT Test.")
 }else{

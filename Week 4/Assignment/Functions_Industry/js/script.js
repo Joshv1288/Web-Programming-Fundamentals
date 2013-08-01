dffe43ba3ad144e7b1f1160588d3a4aa283fc155
Joshua Velez <Joshv1288@gmail.com>
2013-07-31 19:35:06 -1000

    added else if statment

--- Week 4/Assignment/Functions_Industry/js/script.js
+++ Week 4/Assignment/Functions_Industry/js/script.js
@@ -8,18 +8,20 @@ Functions Industry
 
 var p =  prompt("How many pushups did you do?")
 var s = 90
-var t = 85
+var t = 90
 
 testScore(p, s, t)
 
-var testScore = function(p, s, t){
+function testScore(p, s, t){
 	var score = parseInt(p) + parseInt(s) + parseInt(t)
 	return score
 }
 
 
 if (p >= 60 && s >= 60 && t >=60){
-	console.log("You passed your PT Test with a score of " + score)
+	console.log("You passed your PT Test with a score of " + testScore())
+}else if (p >= 90 && s >= 90 && t >=90){
+	console.log("You earned the PT Badge."
 }else{
 	console.log("You failed the PT Test.")
 }

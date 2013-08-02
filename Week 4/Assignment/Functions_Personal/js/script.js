927acbdf0ffd7b33b2e6166cd68744f7629a593b
Joshua Velez <joshv1288@gmail.com>
2013-08-01 17:05:08 -1000

    added comments and alerts

--- Week 4/Assignment/Functions_Personal/js/script.js
+++ Week 4/Assignment/Functions_Personal/js/script.js
@@ -4,16 +4,16 @@
 
 // Determine how long it will take you to save for certain amount
 
-var amountNeeded = prompt("How much do you need to save?")
-var amountSaving = prompt("How much will you be saving?")
-var pay = prompt("How often will you be saving this ammount? i.e Bi = 2 Monthly = 1 Weekly = 4")
+var amountNeeded = prompt("How much do you need to save?"); //Gets amount user needs to save
+var amountSaving = prompt("How much will you be saving?"); // Gets how much user will be saving
+var pay = prompt("How often will you be saving this ammount? i.e Bi = 2 Monthly = 1 Weekly = 4");// Gets how often they will save
 
 var totalSaving = function(){
-	var totalSaving = amountSaving*pay
-	var months = amountNeeded / totalSaving 
-	return months
+	var totalSaving = amountSaving*pay; // calculates the total savings for that month.
+	var months = amountNeeded / totalSaving; //Divdes total saved by the amount needed.
+	return months; // returns how many months needed
 }
 
-console.log("You will be saving for " + totalSaving() + " months");
+alert("You will be saving for " + totalSaving() + " months"); // alerts user of ammount of months need to save the ammount needed
 
-totalSaving() > 6  ? console.log("You will be saving for a long time.") : console.log("Not too long before you will have this.")
+totalSaving() > 6  ? alert("You will be saving for a long time.") : alert("Not too long before you will have this."); //alerts user if they will be saving for a longtime or not

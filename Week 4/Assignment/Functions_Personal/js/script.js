403c96423af7413d67106ec26fdbcc5b1538fe97
Joshua Velez <joshv1288@gmail.com>
2013-08-01 16:51:19 -1000

    testing Personal Function

--- Week 4/Assignment/Functions_Personal/js/script.js
+++ Week 4/Assignment/Functions_Personal/js/script.js
@@ -2,4 +2,15 @@
 //Web Programming Fundamentals
 //Personal Functions
 
-// F
+// Determine how long it will take you to save for certain amount
+
+var amountNeeded = 10000
+var amountSaving = 500
+var pay = 2
+
+function totalSaving(){
+	var totalSaving = amountSaving*pay
+	var months = amountNeeded / totalSaving
+	console.log(months)
+}
+

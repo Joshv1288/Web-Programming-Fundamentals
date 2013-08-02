37d28779f229f60c81e27ed690a360341802e23a
Joshua Velez <joshv1288@gmail.com>
2013-08-01 16:08:08 -1000

    fixed Diameter call

--- Week 4/Assignment/Functions_Wacky/js/script.js
+++ Week 4/Assignment/Functions_Wacky/js/script.js
@@ -10,13 +10,17 @@ var area = 180
 var radius = function(){
 	var pi = 3.14
 	var radius = Math.sqrt(area) / pi
-	console.log(radius)
+	return radius;
 }
 
-radius();
+//radius();
+
+console.log(radius())
 
 function diameter(){
 
-	var diameter = 2*radius
+	var diameter = 2*radius()
 	console.log(diameter)
 }
+
+diameter();

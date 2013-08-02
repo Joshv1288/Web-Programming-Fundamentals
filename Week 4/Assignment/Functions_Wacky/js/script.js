da02770261e3c5dd6b9355d5ca5cc51ed26efc73
Joshua Velez <joshv1288@gmail.com>
2013-08-01 16:02:38 -1000

    testing radius and diameter functions

--- Week 4/Assignment/Functions_Wacky/js/script.js
+++ Week 4/Assignment/Functions_Wacky/js/script.js
@@ -4,3 +4,19 @@
 
 
 //Finding the circumference of the circle
+
+var area = 180
+
+var radius = function(){
+	var pi = 3.14
+	var radius = Math.sqrt(area) / pi
+	console.log(radius)
+}
+
+radius();
+
+function diameter(){
+
+	var diameter = 2*radius
+	console.log(diameter)
+}

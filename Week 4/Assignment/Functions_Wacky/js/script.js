34fdacfbb15951b32bf694e792e523df9e6045fa
Joshua Velez <joshv1288@gmail.com>
2013-08-01 16:34:16 -1000

    added comments and fine tuned code

--- Week 4/Assignment/Functions_Wacky/js/script.js
+++ Week 4/Assignment/Functions_Wacky/js/script.js
@@ -5,27 +5,28 @@
 
 //Finding the circumference of the circle
 
-var area = prompt("Please enter the area of your circle.")
-var pi = 3.14
+var area = prompt("Please enter the area of your circle.") // Prompts user for area
+var unit= prompt("What unit of measure are you using? i.e. feet, inches") //Prompts user for what units they measured in
+var pi = 3.14 // round pi value
 
-var radius = function(){
-	var radius = Math.sqrt(area) / pi
+var radius = function(){ //Finds the radius
+	var radius = Math.sqrt(area) / pi ;//calculates the radius
 	return radius;
 }
 
 
-alert(Math.round(radius()* 100) / 100)
+//alert(Math.round(radius()* 100) / 100)
 
-var diameter = function(){
-	var diameter = 2*radius()
+var diameter = function(){ // finds the diameter
+	var diameter = 2*radius(); // calculates the diameter
 	return diameter
 }
 
-alert(Math.round(diameter() * 100) / 100)
+//alert(Math.round(diameter() * 100) / 100)
 
-var circumference = function(){
-	var circumference = pi*diameter()
+var circumference = function(){ //Finds the circumference
+	var circumference = pi*diameter(); //calculates circumference
 	return circumference
 }
 
-alert(Math.round(circumference() * 100) / 100)
+alert("The circumference of the circle is " + Math.round(circumference() * 100) / 100 + " square " + unit ) //alerts user of the circumference of circle.

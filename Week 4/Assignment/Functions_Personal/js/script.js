8f2f79218134cb8eeb3a80003e2ad2dc68e3cb09
Joshua Velez <joshv1288@gmail.com>
2013-08-01 17:01:36 -1000

    added prompts and semicolons

--- Week 4/Assignment/Functions_Personal/js/script.js
+++ Week 4/Assignment/Functions_Personal/js/script.js
@@ -4,16 +4,16 @@
 
 // Determine how long it will take you to save for certain amount
 
-var amountNeeded = 10000
-var amountSaving = 500
-var pay = 2
+var amountNeeded = prompt("How much do you need to save?")
+var amountSaving = prompt("How much will you be saving?")
+var pay = prompt("How often will you be saving this ammount? i.e Bi = 2 Monthly = 1 Weekly = 4")
 
-function totalSaving(){
+var totalSaving = function(){
 	var totalSaving = amountSaving*pay
-	var months = amountNeeded / totalSaving
-	console.log(months)
+	var months = amountNeeded / totalSaving 
+	return months
 }
 
-totalSaving();
+console.log("You will be saving for " + totalSaving() + " months");
 
 totalSaving() > 6  ? console.log("You will be saving for a long time.") : console.log("Not too long before you will have this.")
